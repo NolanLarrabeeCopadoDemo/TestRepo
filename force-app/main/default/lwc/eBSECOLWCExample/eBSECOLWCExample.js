39c1bfa43bb0a92a66f79c5fe895e6e36dcafe6a
Norm Moy <moy.norm@principal.com>
2025-07-01 20:46:51 +0000

    US-0000029 Norm's User Story (Use Case 1)

---
+++ force-app/main/default/lwc/eBSECOLWCExample/eBSECOLWCExample.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class EBSECOLWCExample extends LightningElement {
+
+
+        clickMe() {
+        console.log('Button clicked!');
+        }
+
+
+}
